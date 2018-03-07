ac6ac9ff5ca10d499ac969f24428993cdb62c48c
scottfwlr <this.specific.scott@gmail.com>
2018-03-07 14:02:23 -0500

    implements reducer logic for handling clearErrors

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -2,13 +2,13 @@ import { merge } from 'lodash';
 import { RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER } from 'actions/session_actions';
 
 
-const sessionReducer = (state = {}, action) => {
+const sessionReducer = (state = {currentUser:null}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return { currentUser: action.currentUser };
     case REMOVE_CURRENT_USER:
-      return {};
+      return { currentUser: null };
     default:
       return state;
   }
