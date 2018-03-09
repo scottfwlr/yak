e0d1daa80e6c9d69b0b9e720a018c214b60271fa
scottfwlr <this.specific.scott@gmail.com>
2018-03-09 10:56:59 -0500

    We are receiving broadcasts from the server

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,14 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      current_user
+    end
+
+    def current_user
+      session_token = cookies.encrypted["_yak_session"]["session_token"]
+      @current_user ||= User.find_by(session_token: session_token)
+    end
   end
 end
--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,14 +1,20 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "main"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    ActionCable.server.broadcast('main', message_from(data))
+  end
+
+  def message_from(data)
+    {
+      text: data['text'],
+      author_id: current_user.id
+    }
   end
 end
-
-
