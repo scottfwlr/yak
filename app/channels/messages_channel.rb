01f3b7e415547aeb2e2ddec05472a2f472d80f30
scottfwlr <this.specific.scott@gmail.com>
2018-03-11 17:26:51 -0400

    Rearranges MessagesChannel message saving

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,4 +1,5 @@
 class MessagesChannel < ApplicationCable::Channel
+
   def subscribed
     stream_from "main"
   end
@@ -8,16 +9,24 @@ class MessagesChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    ActionCable.server.broadcast('main', message_from(data))
+    # check if authorised
+    # ...
+
+    # check if message saved 
+    if message = message_from(data)
+      ActionCable.server.broadcast('main', message)
+    else
+
   end
 
   def message_from(data)
-    # move this!
     message = Message.new(text: data['text'], author: current_user)
     if message.save
       ApplicationController.render(
         partial: 'api/messages/message',
         locals: {message: message})
+    else
+      false
     end
   end
 end
