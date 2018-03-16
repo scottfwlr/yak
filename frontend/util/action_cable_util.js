35b9fdb47a6bd0260738cd391fe8fab714556960
scottfwlr <this.specific.scott@gmail.com>
2018-03-16 10:49:36 -0400

    production setup pt 3

--- frontend/util/action_cable_util.js
+++ frontend/util/action_cable_util.js
@@ -1,4 +1,5 @@
-const createCableSubscribers = (dispatch, channel) => {
+
+const developmentSubscriber = (dispatch, channel) => {
   // App.cable.subscriptions.create({ '' })
 
   App.channels = App.channels || {};
@@ -27,5 +28,47 @@ const createCableSubscribers = (dispatch, channel) => {
     App.channels = {};
   }
 };
+
+
+
+const productionSubscriber = (dispatch, channel) => {
+  // App.cable.subscriptions.create({ '' })
+
+  App.channels = App.channels || {};
+
+  App.subscribeTo = (room) => {
+    App.channels[room] = App.cable.subscriptions.create({ channel, room }, {
+      connected: () => {},
+      disconnected: () => {},
+      received: data => dispatch(JSON.parse(data)),
+      newMessage: function(text) {
+        return this.perform('new_message', { text });
+      },
+      editMessage: function(id, text) {
+        return this.perform('edit_message', { id, text });
+      },
+      deleteMessage: function(id) {
+        return this.perform('delete_message', { id });
+      }
+    });
+  };
+
+  App.unsubscribeAll = () => {
+    Object.values(App.channels).forEach(channel => {
+      channel.unsubscribe();
+    })
+    App.channels = {};
+  }
+};
+
+
+let createCableSubscribers;
+
+if (process.env.NODE_ENV !== 'production') {
+  createCableSubscribers = developmentSubscriber;
+}
+else {
+  createCableSubscribers = productionSubscriber;
+}
 
 export default createCableSubscribers;
